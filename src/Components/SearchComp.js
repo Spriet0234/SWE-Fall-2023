f5cce21a0f8f546912de136e2cb2c8902a3f0434
Santiago P <spriet0234@gmail.com>
2023-10-29 15:40:07 -0500

    a

--- src/Components/SearchComp.js
+++ src/Components/SearchComp.js
@@ -1,9 +1,14 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Item from "./Item";
 
 export default function SearchComp() {
   const location = useLocation();
-  const filteredItems = location.state?.filteredItems || [];
+  const initialItems = location.state?.filteredItems || [];
+
+  const [filteredItems, setFilteredItems] = useState(initialItems);
+  const [priceFilter, setPriceFilter] = useState(""); // "low-to-high" or "high-to-low"
+  const [availableFilter, setAvailableFilter] = useState(false); // true for available items only, false for all
 
   // You can now use the filteredItems in your component
   const onProductClick = (itemName) => {
@@ -11,9 +16,49 @@ export default function SearchComp() {
     // Add additional logic here for when an item is clicked
   };
   console.log(filteredItems);
+  useEffect(() => {
+    let newFilteredItems = [...initialItems];
+
+    if (priceFilter === "low-to-high") {
+      newFilteredItems.sort((a, b) => a.price - b.price);
+    } else if (priceFilter === "high-to-low") {
+      newFilteredItems.sort((a, b) => b.price - a.price);
+    }
+
+    if (availableFilter) {
+      newFilteredItems = newFilteredItems.filter((item) => item.available);
+    }
+
+    setFilteredItems(newFilteredItems);
+  }, [priceFilter, availableFilter]);
+  function toggleDropdown() {
+    const dropdownContent = document.querySelector(".dropdown-content");
+    dropdownContent.style.display =
+      dropdownContent.style.display === "none" ? "block" : "none";
+  }
 
   return (
     <div>
+      <div>
+        <label>Sort by price:</label>
+        <select
+          value={priceFilter}
+          onChange={(e) => setPriceFilter(e.target.value)}
+        >
+          <option value="">Select...</option>
+          <option value="low-to-high">Low to High</option>
+          <option value="high-to-low">High to Low</option>
+        </select>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={availableFilter}
+            onChange={(e) => setAvailableFilter(e.target.checked)}
+          />
+          Show available items only
+        </label>
+      </div>
       <div>Results:</div>
       <section style={{ display: "flex", flexWrap: "wrap", margin: 10 }}>
         {filteredItems.map((item) => (

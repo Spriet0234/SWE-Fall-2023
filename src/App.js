3fcda3710ec103a6a06c10b2cdeb747ee7d13a41
tylerclifton3 <100748044+tylerclifton3@users.noreply.github.com>
2023-11-09 19:12:34 -0600

    Added API GET for Items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaShoppingBag } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
@@ -34,81 +34,47 @@ import {
 import ItemDetails from "./Components/ProductDetails";
 import SearchBar from "./Components/SearchBar";
 import SearchComp from "./Components/SearchComp";
+import axios from "axios"; // Import axios for making HTTP requests
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      name: "T-Shirt",
-      price: 18.99,
-      quantity: 1,
-      description:
-        "A classic wardrobe staple, this t-shirt is made from breathable, 100% cotton fabric with a ribbed crew neck and short sleeves. Its timeless design ensures comfort and versatility for everyday wear.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    {
-      id: 2,
-      name: "Jeans",
-      price: 49.99,
-      quantity: 1,
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-      description:
-        "These jeans offer a sleek and slimming fit, thanks to their stretchy denim fabric and deep indigo wash. The quality craftsmanship and classic design make them a reliable choice for your casual and semi-formal occasions.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    {
-      id: 3,
-      name: "Shorts",
-      price: 15.99,
-      quantity: 1,
+  const fetchItems = async () => {
+    try {
+      const response = await axios.get(
+        "https://fzp30pyckc.execute-api.us-east-1.amazonaws.com/dev"
+      );
 
-      description:
-        "Embrace warm weather with these comfortable shorts. Crafted from lightweight, durable cotton, they feature a relaxed fit, side pockets, and a versatile design that's perfect for outdoor adventures or leisurely days.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    {
-      id: 4,
-      name: "Dress",
-      price: 39.99,
-      quantity: 0,
+      // Parse the JSON string in the body property
+      const responseBody = JSON.parse(response.data.body);
 
-      description:
-        "Elevate your style with this elegant dress, boasting a figure-flattering A-line silhouette, delicate lace detailing, and a modest V-neck. Whether for special occasions or casual gatherings, this dress effortlessly combines sophistication and charm.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    {
-      id: 5,
-      name: "Turtle neck",
-      price: 39.99,
-      quantity: 1,
-      description:
-        "Stay cozy and stylish in this turtleneck sweater. Made from soft, breathable fabric, it features a ribbed collar and cuffs for extra warmth. The classic turtleneck design adds a touch of timeless elegance to your winter wardrobe.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    {
-      id: 6,
-      name: "Chinos",
-      price: 39.99,
-      quantity: 0,
+      console.log("Response Body:", responseBody);
 
-      description:
-        "Achieve a polished look with these chinos, crafted from high-quality cotton twill. Their tailored fit, clean lines, and versatile khaki color make them a reliable choice for both business-casual and smart-casual attire.",
-      image:
-        "https://media.istockphoto.com/id/1354020635/photo/white-t-shirt-mockup-front-used-as-design-template-tee-shirt-blank-isolated-on-white.webp?b=1&s=170667a&w=0&k=20&c=X5WLEp3EiqJ7eQqGQPW6Vx8l7joo56cTsCmSOIaAfPI=",
-    },
-    // ... more items
-  ]);
-  const addItem = (newItem) => {
-    setItems((prevItems) => {
-      return [...prevItems, { ...newItem, id: prevItems.length + 1 }];
-    });
-    console.log(items);
+      // Move the setItems call outside the fetchItems function
+      setItems(responseBody.Items);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+      setLoading(false);
+    }
   };
+
+  useEffect(() => {
+    // Call fetchItems inside a separate function to use async/await
+    const fetchData = async () => {
+      await fetchItems();
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs only once, similar to componentDidMount
+
+  const addItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, { ...newItem, id: prevItems.length + 1 }]);
+  };
+
+  console.log("Items in App component:", items);
+
   return (
     <AuthProvider>
       <CartProvider>
